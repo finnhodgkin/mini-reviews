294adcc41212f7bd12aef7289fe408f57424ca5f
finnhodgkin <finnhodgkin@gmail.com>
2017-08-13 21:25:13 +0100

    adds copy-able button link

--- src/handlers/badges.js
+++ src/handlers/badges.js
@@ -7,7 +7,9 @@ const addAReview = require('./../database/addAReview');
 module.exports = async (req, reply) => {
   try {
     const badges = await rf('./public/badge.html', { encoding: 'utf8' });
-    const html = badges.replace(/<!--REPLACEME-->/g, req.params.name);
+    const html = badges
+      .replace(/<!--REPLACEME-->/g, req.params.name)
+      .replace(/<!--HOST-->/g, process.env.HOSTNAME);
     reply(html);
   } catch (err) {
     reply('error loading html');
