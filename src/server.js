d53a8a87c36d767e5ead924cdf72a440b00b3da9
finnhodgkin <finnhodgkin@gmail.com>
2017-08-13 21:43:32 +0100

    adds port environment variable

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ require('env2')('.env');
 
 const server = new hapi.Server();
 
-server.connection({ port: 4000 });
+server.connection({ port: process.env.PORT || 4000 });
 
 server.register([inert, cookieAuthModule, contextCredentials], err => {
   if (err) throw err;
